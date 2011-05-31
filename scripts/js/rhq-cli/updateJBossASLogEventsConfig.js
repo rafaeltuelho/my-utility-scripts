dc6153fbb9923c8dad9e02ba7e76b6445ac3273c
Rafael Soares <rafaelcba@gmail.com>
2011-05-31 13:56:31 -0300

    some enhancements

--- scripts/js/rhq-cli/updateJBossASLogEventsConfig.js
+++ scripts/js/rhq-cli/updateJBossASLogEventsConfig.js
@@ -13,45 +13,97 @@
  *    RHQ-CLI_HOME/bin/rhq-cli.sh -u rhqadmin -p rhqadmin -s 127.0.0.1 -t 7080 -f /<full path to this script>/script.js
  *
  * Author: rafaelcba at gmail dot com
+ * TODO: parameterize the data entry: Group Name, new PropertySimple values, etc 
  */
 
 
+println("START! ");
+
+// Get the Resource Group by its Name
 var rgc = new ResourceGroupCriteria();
-rgc.addFilterName("JBossAS5-Instances");
+rgc.addFilterName("DynaGroup - ALL-JBossAS4");
 rgc.fetchExplicitResources(true);
+
 var jbossGroupList = ResourceGroupManager.findResourceGroupsByCriteria(rgc);
 rgc.fetchExplicitResources(true);
+
 var jbossGroup = jbossGroupList.get(0);
 var jbossResourcesArray = jbossGroup.explicitResources.toArray();
 
+// util vars
+// only Y|y|N|n is accepted as answer 
+var ASN_REGEX = java.util.regex.Pattern.compile("^[YynN]$");
+
+// for each JBossAS resource instance foud in the Group
 for( i in jbossResourcesArray ) {
-   var res = jbossResourcesArray[i];
-   var jboss = ProxyFactory.getResource(res.id);
-   var avail  = AvailabilityManager.getCurrentAvailabilityForResource(jboss.id);
 
-   println("");
-   println("");
-   println("  " + jboss.name );
-   println("    - Availability: " + avail.availabilityType.getName());
-   println("    - Started     : " + avail.startTime.toGMTString());
-   println("");
+   try {
+      var res = jbossResourcesArray[i];
+      var jboss = ProxyFactory.getResource(res.id);
+      var avail  = AvailabilityManager.getCurrentAvailabilityForResource(jboss.id);
 
-   var jbossConfig = ConfigurationManager.getPluginConfiguration(jboss.id);
-   var jbossConfigPropList = jbossConfig.getList("logEventSources");
-   var jbossConfigLogEventSourcesMap = jbossConfigPropList.list.get(0);
+      println("");
+      println("");
+      println("  " + jboss.name );
+      println("    - Availability: " + avail.availabilityType.getName());
+      println("    - Started     : " + avail.startTime.toGMTString());
+      println("");
 
-   var jbossConfigLogEventSourcesMapPropertySimpleEnabled = jbossConfigLogEventSourcesMap.getSimple("enabled");
-   jbossConfigLogEventSourcesMapPropertySimpleEnabled.setBooleanValue(true);
+      // test if the resource is UP
+      if (avail.availabilityType.equals(AvailabilityType.UP)) {      
+       
+         var jbossConfig = ConfigurationManager.getPluginConfiguration(jboss.id);
+         var jbossConfigPropList = jbossConfig.getList("logEventSources");
 
-   var jbossConfigLogEventSourcesMapPropertySimpleIncludesPattern = jbossConfigLogEventSourcesMap.getSimple("includesPattern");
-   jbossConfigLogEventSourcesMapPropertySimpleIncludesPattern.setStringValue(".*SQLException.*");
+         // if there is one or more server log defined
+         println(" ---> " + jbossConfigPropList.list.size());
+         if (jbossConfigPropList.list.size() > 0) {
+            println(" UPDATING [" + jboss.name + "] ...");
 
-   var jbossConfigLogEventSourcesMapPropertySimpleMinimumSeverity = jbossConfigLogEventSourcesMap.getSimple("minimumSeverity");
-   jbossConfigLogEventSourcesMapPropertySimpleMinimumSeverity.setStringValue("error");
+            // for each log defined fors this JBossAS instance
+            for ( l in jbossConfigPropList.list.toArray() ) {
+               var jbossConfigLogEventSourcesMap = jbossConfigPropList.list.get(l);
+               var jbossConfigLogEventSourcesMapPropertySimpleLogPath = jbossConfigLogEventSourcesMap.getSimple("logFilePath");
+               println("   >>>  enabling [" + jbossConfigLogEventSourcesMapPropertySimpleLogPath.getStringValue() + "] <<<");
 
-   ConfigurationManager.updatePluginConfiguration(jboss.id, jbossConfig);
+               var jbossConfigLogEventSourcesMapPropertySimpleEnabled = jbossConfigLogEventSourcesMap.getSimple("enabled");
+               jbossConfigLogEventSourcesMapPropertySimpleEnabled.setBooleanValue(true);
 
-   var latestPluginConfigurations = ConfigurationManager.getLatestPluginConfigurationUpdate(jboss.id);
-   println("latestPluginConfigurations: " + latestPluginConfigurations.toString());
+               var jbossConfigLogEventSourcesMapPropertySimpleIncludesPattern = jbossConfigLogEventSourcesMap.getSimple("includesPattern");
+               jbossConfigLogEventSourcesMapPropertySimpleIncludesPattern.setStringValue(".*SQLException*|.*ORA-\d{4,5}|.*PSQLException|.*JBossResourceException: Could not create connection|.*Caused by: java.net.ConnectException: Connection timed out|.*javax.resource.ResourceException: Unable to get managed connection for*|.*java.io.IOException:*|.*UnknownHostException*");
+
+               var jbossConfigLogEventSourcesMapPropertySimpleMinimumSeverity = jbossConfigLogEventSourcesMap.getSimple("minimumSeverity");
+               jbossConfigLogEventSourcesMapPropertySimpleMinimumSeverity.setStringValue("error");
+
+               ConfigurationManager.updatePluginConfiguration(jboss.id, jbossConfig);
+
+               var latestPluginConfigurations = ConfigurationManager.getLatestPluginConfigurationUpdate(jboss.id);
+               println("   latestPluginConfigurations: " + latestPluginConfigurations.toString());
+            }
+         }
+      }
+      else {
+         continue;
+      }
+   } 
+   catch ( ex ) {
+      println("   --> Caught " + ex );
+   }
+   finally {
+      
+      println(" "); 
+      // println(" >>> [" + jboss.name  + "] log events config UPDATED! <<<");
+      println(" Do you want to continue? (Y/n)");
+
+      var console = new java.util.Scanner(java.lang.System["in"]);
+      if ( console.hasNext(ASN_REGEX) && console.next().equalsIgnoreCase("Y") )
+         continue;
+      else
+         break;
+
+      console.close();
+
+   }
 }
 
+println("FINISH! ");
